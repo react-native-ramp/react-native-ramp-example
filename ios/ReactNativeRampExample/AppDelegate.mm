02c88eb945025c14002af1231d473b1e12123777
react-native-ramp <support@react-native-ramp.dev>
2023-02-23 17:08:25 +0000

    Configure iOS project

--- ios/ReactNativeRampExample/AppDelegate.mm
+++ ios/ReactNativeRampExample/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <RampSDK.h>
 
 @implementation AppDelegate
 
@@ -16,11 +17,7 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
+  return [RampSDK sourceURLForBridge:bridge];
 }
 
 /// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
